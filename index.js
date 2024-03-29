032c4bf2598ca769975121105d321d628a5d45fa
Anes Belfodil <abelfodil@users.noreply.github.com>
2024-03-29 18:40:51 +0000

    Add some stuff

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+import 'dotenv/config';
+import {IgApiClient} from 'instagram-private-api';
+
+const ig = new IgApiClient();
+
+ig.state.generateDevice(process.env.IG_USERNAME);
+const auth = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+console.log(JSON.stringify(auth));
+
+const targetUser = await ig.user.searchExact('username');
+console.log(targetUser)
